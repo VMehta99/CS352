66e465e620be37bd0a93512ae474b580152f567a
Vedant <vm.WorkAddress@gmail.com>
2020-10-01 21:31:40 -0400

    improved functionality

--- Project1/doc_root/serverThread.java
+++ Project1/doc_root/serverThread.java
@@ -12,6 +12,7 @@ public class serverThread extends Thread {
     public serverThread(Socket client)
     {
         this.client = client;
+
         try{
             inFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
             outToClient = new DataOutputStream(client.getOutputStream());
@@ -25,23 +26,30 @@ public class serverThread extends Thread {
     public void run() {
         String request;
         String[] requestParts;
+
         try {
             request = inFromClient.readLine();
         } catch (IOException e) {
-            System.out.println("Error reading from file:\n" + e.getStackTrace());
             sendErrorCode(500);
             return;
         }
+        
+        if(request==null){
+            sendErrorCode(400);
+            return;
+        }
+
+        //Extract each part of the request
         requestParts = request.split(" ");
-
-        String method = requestParts[0];
-        String file = requestParts[1];
-
-
+        
+        //If the request does not contain all relevant parts, 400 Bad Request
         if(requestParts.length != 3){
             sendErrorCode(400);
             return;
         }
+
+        String method = requestParts[0];
+        String file = requestParts[1];
 
         isValidMethod(requestParts[0]);
 
@@ -70,7 +78,7 @@ public class serverThread extends Thread {
         }
 
         //Check if one of the 3valid commands
-        if(!method.equals("GET") && ! method.equals("POST") && !method.equals("HEAD"))
+        if(!method.equals("GET") && !method.equals("POST") && !method.equals("HEAD"))
         {
             sendErrorCode(501);
             return false;
@@ -94,23 +102,66 @@ public class serverThread extends Thread {
         503    Service Unavailable
         505    HTTP Version Not Supported
     
-        Function should take in a code and print its corresponding message:
+        Function should take in a codet and print its corresponding message:
         FORMAT:  
            - String message = String.format("HTTP/1.0 %s %s", code, ErrorMessage);
 
     */
     public void sendErrorCode(int code)
     {
-        // System.out.printf("Error code: %d %s\n", code, "yer");
+        String ErrorMessage="";
+        switch(code){
+            case 200:
+                ErrorMessage="OK";
+                break;
+            case 304:
+                ErrorMessage="Not Modified";
+                break;
+            case 400:
+                ErrorMessage="Bad Request";
+                System.out.println("fuck you");
+                break;
+            case 403:
+                ErrorMessage="Forbidden";
+                break;
+            case 404:
+                ErrorMessage="Not Found";
+                break;
+            case 408:
+                ErrorMessage="Request Timeout";
+                break;
+            case 500:
+                ErrorMessage="Internal Server Error";
+                break;
+            case 501:
+                ErrorMessage="Not Implemented";
+                break;
+            case 503:
+                ErrorMessage="Service Unavailable";
+                break;
+            case 505:
+                ErrorMessage="HTTP Version Not Supported";
+                break;
+        }
+
         String message = String.format("HTTP/1.0 %s %s", code, ErrorMessage);
+
         try{
-            outToClient.writeBytes(message + "\n");
+            System.out.println(message);
+            outToClient.writeBytes(message);
+            outToClient.flush();
+
         }catch(IOException e){
             System.out.printf("Error sending response to client");
         }
         close();
     }
 
+
+    /*
+        Void method for organization
+        Just closes the socket. 
+    */
     public void close()
     {
         try {
